a7c82c5142534a154e9cb27a01a168dbe0f30edb
poc7667 <poc7667@gmail.com>
2014-09-06 10:35:41 +0800

    Fix: asset_path not load error

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,11 @@ module JimmySawaWedding
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << "#{Rails.root}/vendor/themes"
+        config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
+    # Precompile *all* assets, except those that start with underscore
+    config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,13 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile += %w( bootstrap.css )
+  # config.assets.precompile += %w( vendor/themes/* )
+  config.assets.precompile += %w( vendor/themes/**/application.js )
+  config.assets.precompile += %w( vendor/themes/**/application.css )
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
+  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
 
   # Generate digests for assets URLs.
   config.assets.digest = true
