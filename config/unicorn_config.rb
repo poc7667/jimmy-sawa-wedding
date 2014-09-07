71212926e57fd101812fc206f86c7ab15a0f3b07
poc7667 <poc7667@gmail.com>
2014-09-07 22:52:21 +0800

    Add unicorn config file

---
+++ config/unicorn_config.rb
@@ -0,0 +1,50 @@
+#refer https://www.digitalocean.com/community/tutorials/how-to-deploy-rails-apps-using-unicorn-and-nginx-on-centos-6-5
+#https://devcenter.heroku.com/articles/rails-unicorn
+# Set the working application directory
+# working_directory "/path/to/your/app"
+require 'pathname'
+require 'pry'
+
+config_file_folder = File.expand_path(File.dirname(__FILE__))
+app_name='jimmy-sawa-wedding'
+working_directory = File.expand_path("..", config_file_folder)
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "#{working_directory}/tmp/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "#{working_directory}/log/unicorn_err.log"
+stdout_path "#{working_directory}/log/unicorn_std.log"
+
+# Unicorn socket
+socket_file = "/tmp/unicorn.#{app_name}.sock"
+print "socket_file: #{socket_file}"
+listen socket_file, backlog: 1024
+listen 8081, tcp_nopush: false
+
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 5)
+timeout 300
+preload_app true
+
+before_fork do |server, worker|
+    Signal.trap 'TERM' do
+        puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+        Process.kill 'QUIT', Process.pid
+    end
+
+    defined?(ActiveRecord::Base) and
+        ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+    Signal.trap 'TERM' do
+        puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    end
+
+    defined?(ActiveRecord::Base) and
+        ActiveRecord::Base.establish_connection
+end
+
